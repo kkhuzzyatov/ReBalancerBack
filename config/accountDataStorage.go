52d19d0f8c2184ed7e02e69a3f188da0f13ae7e3
Kamil Khuzzyatov <kamil.khuzzyatov@list.ru>
2025-02-27 13:27:32 +0300

    stub_2

---
+++ config/accountDataStorage.go
@@ -0,0 +1,20 @@
+package config
+
+import "fmt"
+
+func GetTokenOfTBank() string {
+	return ""
+}
+
+func GetTokenOfTelegram() string {
+	return ""
+}
+
+func GetAccountID() []string {
+	return []string{"2179550658"}
+}
+
+func GetDBConnStr() string {
+	connString := fmt.Sprintf("")
+	return connString
+}
